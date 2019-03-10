44c90f255edac1dd3ea9ba11365cb2462fc00a01
maininbule <leo@LeodeMacBook-Pro.local>
2019-03-10 17:54:15 +0800

    Site updated: 2019-03-10 17:54:15

--- js/main.js
+++ js/main.js
@@ -1,45 +1,71 @@
-(function($){
-    $('.article img:not(".not-gallery-item")').each(function () {
-        // wrap images with link and add caption if possible
-        if ($(this).parent('a').length === 0) {
-            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
-            if (this.alt) {
-                $(this).after('<div class="has-text-centered is-size-6 has-text-grey caption">' + this.alt + '</div>');
-            }
-        }
+/*!
+ * Clean Blog v1.0.0 (http://startbootstrap.com)
+ * Copyright 2015 Start Bootstrap
+ * Licensed under Apache 2.0 (https://github.com/IronSummitMedia/startbootstrap/blob/gh-pages/LICENSE)
+ */
+
+// Tooltip Init
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+// responsive tables
+$(document).ready(function() {
+    $("table").each(function(){
+      if ($(this).parent().get(0).tagName != 'FIGURE') {
+        $(this).addClass("table table-responsive table-striped table-hover");
+        $(this).find("th").addClass("text-center");
+      }
     });
+});
 
-    if (typeof(moment) === 'function') {
-        $('.article-meta time').each(function () {
-            $(this).text(moment($(this).attr('datetime')).fromNow());
-        });
+// responsive embed videos
+$(document).ready(function () {
+    $('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+    $('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
+    $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+    $('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
+    $('img').addClass('img-responsive-center')
+});
+
+// whether a post
+function isPages(attr){
+    var currentBoolean = document.querySelector('.navbar.header-navbar').getAttribute(attr);
+    if(currentBoolean === 'true'){return true;}
+    return false;
+}
+/*
+    scroll function
+    3 parameters
+        1. a DOM object
+        2 a class for targeted object
+        3 height when acctivated (optional. default: the height of the DOM)
+*/
+function scrollCheck(scrollTarget, toggleClass, scrollHeight){
+    document.addEventListener('scroll',function(){
+    var currentTop = window.pageYOffset;
+        currentTop > (scrollHeight||scrollTarget.clientHeight)
+        ?scrollTarget.classList.add(toggleClass)
+        :scrollTarget.classList.remove(toggleClass)
+    })
+}
+
+
+
+/*
+* Steps
+* 1. get the content of h1
+* 2. scroll and appear fixed navbar
+* 3. the content of h1 is shown center at the top of the page
+* */
+
+(function(){
+    if (isPages('data-ispost')){
+        var navbar = document.querySelector('.navbar-custom');
+        var introHeader = document.querySelector('.intro-header').offsetHeight;
+        var introHeader = introHeader > 597 ? introHeader : 500;
+        var toc = document.querySelector('.toc-wrap');
+        scrollCheck(toc,'toc-fixed',introHeader-60);
+        // scrollCheck(navbar,'is-fixed');
     }
-
-    function adjustNavbar() {
-        const navbarWidth = $('.navbar-main .navbar-start').outerWidth() + $('.navbar-main .navbar-end').outerWidth();
-        if ($(document).outerWidth() < navbarWidth) {
-            $('.navbar-main .navbar-menu').addClass('is-flex-start');
-        } else {
-            $('.navbar-main .navbar-menu').removeClass('is-flex-start');
-        }
-    }
-    adjustNavbar();
-    $(window).resize(adjustNavbar);
-
-    var $toc = $('#toc');
-    if ($toc.length > 0) {
-    var $mask = $('<div>');
-        $mask.attr('id', 'toc-mask');
-
-        $('body').append($mask);
-
-        function toggleToc() {
-            $toc.toggleClass('is-active');
-            $mask.toggleClass('is-active');
-        }
-
-        $toc.on('click', toggleToc);
-        $mask.on('click', toggleToc);
-        $('.navbar-main .catalogue').on('click', toggleToc);
-    }
-})(jQuery);
+})();
